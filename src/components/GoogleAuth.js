8e578c61ba395c41ab904226aa0c3ad8824c1eee
tcarmody90 <tcarmody90@yahoo.com>
2021-03-16 17:43:28 -0500

    Refactor form to be reusable.

--- src/components/GoogleAuth.js
+++ src/components/GoogleAuth.js
@@ -7,7 +7,7 @@ class GoogleAuth extends React.Component {
     window.gapi.load("client:auth2", () => {
       window.gapi.client
         .init({
-          clientId: "",
+          clientId: "CLIENT_ID",
           prompt: "select_account",
           scope: "email",
         })
--- src/components/streams/StreamEdit.js
+++ src/components/streams/StreamEdit.js
@@ -1,7 +1,27 @@
 import React from "react";
+import { connect } from "react-redux";
+import { fetchStream } from "../../actions";
 
-const StreamEdit = () => {
-  return <div>StreamEdit</div>;
+class StreamEdit extends React.Component {
+  componentDidMount() {
+    this.props.fetchStream(this.props.match.params.id);
+  }
+  render() {
+    if (!this.props.stream) {
+      return <div>Loading...</div>;
+    }
+
+    return (
+      <div>
+        <h2>{this.props.stream.title}</h2>
+        <h3>{this.props.stream.description}</h3>
+      </div>
+    );
+  }
+}
+
+const mapStateToProps = (state, ownProps) => {
+  return { stream: state.streams[ownProps.match.params.id] };
 };
 
-export default StreamEdit;
+export default connect(mapStateToProps, { fetchStream })(StreamEdit);
